d368592b394aabbbff9f1038339fdaec0361ca7f
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-01 20:59:49 +0100

    Small improvement

--- disgenet-rett/get-rett-nanopub-uris.sh
+++ disgenet-rett/get-rett-nanopub-uris.sh
@@ -1,4 +1,4 @@
-rm files/rett-nanopub-uris.txt
+rm -f files/rett-nanopub-uris.txt
 APIURL='http://petapico.d2s.labs.vu.nl/api/database/api.php'
 SEARCHURI='http%3A%2F%2Flinkedlifedata.com%2Fresource%2Fumls%2Fid%2FC0035372'
 wget -O - "$APIURL?search-uri=$SEARCHURI&page=1&assertion=on&format=text" \
