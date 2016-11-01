c4d014b2e628c0a64592a2f63f4dc3dddb209896
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-01 17:52:48 +0100

    Add script to generate Proteinatlas index

---
+++ proteinatlas/make_proteinatlas_index.sh
@@ -0,0 +1,8 @@
+np mkindex \
+  -o files/index.proteinatlas.trig \
+  -t "Human Protein Atlas data" \
+  -d "Nanopublications from the Human Protein Atlas" \
+  -a http://www.proteinatlas.org \
+  -c 0000-0002-1267-0234 \
+  files/trusty.proteinatlas.trig
+
