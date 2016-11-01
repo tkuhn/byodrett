bee46357e1ab48c40eb38891df5e7c232a5c8cce
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-01 17:22:26 +0100

    Add Proteinatlas files

---
+++ proteinatlas/README.md
@@ -0,0 +1,4 @@
+Nanopublications:
+
+    $ np check files/trusty.proteinatlas-transformed.trig 
+    Summary: 1254468 trusty (unsigned);
--- proteinatlas/transform.sh
+++ proteinatlas/transform.sh
@@ -1,4 +1,4 @@
-cat proteinatlas.trig \
+cat files/proteinatlas.trig \
   | sed -r 's/^(:.*) a np:Nanopublication .$/\1_head {\n\1 a np:Nanopublication ./' \
   | sed 's/^\(:.*\)_assertion {\s*$/}\n\1_assertion {/' \
-  > proteinatlas-transformed.trig
+  > files/proteinatlas-transformed.trig
