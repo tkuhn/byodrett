c084e63c9efe05fc9c5ecbe4e80105dba907caa1
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 09:05:43 +0100

    Correct mistake with DisGeNET subset, now using version v4.0.0

--- disgenet-rett/README.md
+++ disgenet-rett/README.md
@@ -1,8 +1,7 @@
 DisGeNET nanopubs about Rett Syndrome
 =====================================
 
-Getting the subset of DisGeNET nanopubs that are about the Rett syndrome.
+Getting the subset of DisGeNET v4.0.0 nanopubs that are about the Rett syndrome.
 
-Resulting nanopub index (1284 nanopubs):
-http://np.inn.ac/RAhoIw-XmyVv7DfMKyni67OHBFRqPvHAodr_iRgJxhtxY
-
+Resulting nanopub index (968 nanopubs):
+http://np.inn.ac/RAwMqPOC0ZdZquSGih3aNsUuwGAGGtaCR3L4r5IRkK5x0
--- disgenet-rett/make-index.sh
+++ disgenet-rett/make-index.sh
@@ -1,6 +1,6 @@
 np mkindex \
-  -t "Rett Syndrome data from DisGeNET v3.0.0.0" \
-  -d "This is a subset of DisGeNET v3.0.0.0 with all its nanopublications about the Rett Syndrome (UMLS:C0035372)." \
+  -t "Rett Syndrome data from DisGeNET v4.0.0" \
+  -d "This is a subset of DisGeNET v4.0.0 with all its nanopublications about the Rett Syndrome (UMLS:C0035372)." \
   -c "http://orcid.org/0000-0002-1267-0234" \
-  -o files/rett-nanopubs-disgenet3.index.trig \
-  files/rett-nanopubs-disgenet3.trig
+  -o files/rett-nanopubs-disgenet4.index.trig \
+  files/rett-nanopubs-disgenet4.trig
