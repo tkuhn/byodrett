53480e2bdbf7f260330e55c157c0bb20de32a9ac
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 15:43:58 +0100

    Make indexes

---
+++ nanopubs/make-indexes.sh
@@ -0,0 +1,11 @@
+$ np mkindex -o lists/rett.index.trig lists/rett.txt
+Index URI: http://np.inn.ac/RAwepzy1rtZCwp_ABboUHReraz95kc4-EM-RP6V4RkKzc
+
+$ np mkindex -o lists/mecp2.index.trig lists/mecp2.txt
+Index URI: http://np.inn.ac/RAK92fVJCMpJ3Ie3jMXiehtKnFNzvGd3MQnXOj55CgML8
+
+$ np mkindex -o lists/cdkl5.index.trig lists/cdkl5.txt 
+Index URI: http://np.inn.ac/RAtyhkWi1DEMnlombfdWvO4zqi2OgCoNz3MRy-YucWH2Q
+
+$ np mkindex -o lists/foxg1.index.trig lists/foxg1.txt 
+Index URI: http://np.inn.ac/RA0ImMGdhttIJxDJsPw7HiRLZiqpIIhRhzw0Y6r8LvmKg
