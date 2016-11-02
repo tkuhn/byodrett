269385d4d7c6c2d8c71b68526e73ec45a74fdc08
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 16:55:56 +0100

    Small changes

--- proteinatlas/make_proteinatlas_index.sh
+++ proteinatlas/make_proteinatlas_index.sh
@@ -3,6 +3,4 @@ np mkindex \
   -t "Human Protein Atlas data" \
   -d "Nanopublications from the Human Protein Atlas" \
   -a http://www.proteinatlas.org \
-  -c 0000-0002-1267-0234 \
   files/trusty.proteinatlas.trig
-
