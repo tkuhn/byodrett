61e9f87b95a37aaa4795d459c34bbc7ca53d60f7
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 11:29:47 +0100

    Get MECP2 nanopubs

---
+++ mecp2-search/get-mecp2-uris.sh
@@ -0,0 +1,9 @@
+mkdir -p lists
+rm lists/*.txt
+
+cat mecp2-uris.txt \
+  | echo "$(perl -MURI::Escape -e 'foreach $l ( <STDIN> ) { print uri_escape($l) . "\n"; }' )" \
+  | awk '{ print "wget -O lists/"$1".txt \047http://petapico.d2s.labs.vu.nl/api/database/api.php?search-uri="$1"&page=1&assertion=on&format=text\047" }' \
+  | bash
+
+find lists/ -empty -type f -delete
