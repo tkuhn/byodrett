3ebaca1468fa57e9f5414d84ce800a87dc457989
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 11:54:34 +0100

    Make sub-folder

--- mecp2-search/get-mecp2-uris.sh
+++ mecp2-search/get-mecp2-uris.sh
@@ -1,8 +1,8 @@
-mkdir -p lists
+mkdir -p lists/mecp2
 
 cat mecp2-uris.txt \
   | echo "$(perl -MURI::Escape -e 'foreach $l ( <STDIN> ) { print uri_escape($l) . "\n"; }' )" \
-  | awk '{ print "wget -O lists/"$1".txt \047http://petapico.d2s.labs.vu.nl/api/database/api.php?search-uri="$1"&page=1&assertion=on&format=text\047" }' \
+  | awk '{ print "wget -O lists/mecp2/"$1".txt \047http://petapico.d2s.labs.vu.nl/api/database/api.php?search-uri="$1"&page=1&assertion=on&format=text\047" }' \
   | bash
 
-find lists/ -empty -type f -delete
+find lists/mecp2 -empty -type f -delete
