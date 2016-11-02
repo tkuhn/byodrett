552a44042886838fc931991200c2ae7ea4f7f178
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 09:48:03 +0100

    Add script for filtering

---
+++ disgenet-rett/filter-disgenet-versions.sh
@@ -0,0 +1,14 @@
+npop filter \
+  -f 'v2.1.0' \
+  -o files/rett-nanopubs-disgenet2.trig \
+  files/rett-nanopubs.trig
+
+npop filter \
+  -f 'v3.0.0' \
+  -o files/rett-nanopubs-disgenet3.trig \
+  files/rett-nanopubs.trig
+
+npop filter \
+  -f 'v4.0.0' \
+  -o files/rett-nanopubs-disgenet4.trig \
+  files/rett-nanopubs.trig
--- disgenet-rett/get-rett-nanopubs.sh
+++ disgenet-rett/get-rett-nanopubs.sh
@@ -1,2 +1,3 @@
+# This is ugly and slow (improve!):
 rm -f files/rett-nanopub.trig
 cat files/rett-nanopub-uris.txt | awk '{ print "np get "$1" >> files/rett-nanopubs.trig" }' | bash
