502e110a9d7e3f8b305adc3528e19a70eaf2a73f
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-03 10:32:06 +0100

    Add missing statement

--- triplestore/virtuoso/load-input-files.sh
+++ triplestore/virtuoso/load-input-files.sh
@@ -2,4 +2,5 @@ echo "# Run the following commands:"
 echo "isql-v -U dba -P dba"
 echo "SQL> ld_dir('dumps', '*.nq.gz', 'http://www.example.com/default-graph');"
 echo "SQL> rdf_loader_run();"
+echo "SQL> checkpoint;"
 docker exec -it my-virtuoso bash
